8a9fce8bbc6182f14ab5660cd36f4e8841202696
Luterad <100513212+Luterad@users.noreply.github.com>
2024-12-06 11:23:07 +0300

    Update control.gd

--- control.gd
+++ control.gd
@@ -1,50 +1,49 @@
-class_name FlowBar extends Resource
-
-## Smooth range bar flow resource.
-## 
-## This resource allows you to smoothly (in fact anyway how you'll want) degrees [code]value[/code] of your [member bar_flow] by [member curve].
+extends Control
 
 @export var curve: Curve
 
 @export_group("Nodes")
 @export_subgroup("Timers", "timer_")
-@onready var timer_await: Timer ## When runs out [member bar_flow]'s [code]value[/code] starts to degrees according to [member curve].
-@onready var timer_flow: Timer ## While runs sets [member bar_flow]'s [code]value[/code] according to [member curve]'s [code]X[/code].
+@export var timer_await: Timer
+@export var timer_flow: Timer
 
 @export_subgroup("Bars", "bar_")
-@onready var bar_basic: Range ## Bar for basic changing of it's value.
-@onready var bar_flow: Range ## Bar for smooth changing of it's value.
+@export var bar_hp: Range
+@export var bar_flow_hp: Range
 
-var value: int = 100: set = set_new_value
+var hp: int = 100: set = set_hp
 var incurve: Curve
 
-func _enter_tree() -> void: timer_await.timeout.connect(_start_flow)
+func set_hp(new_hp) -> void:
+	if hp > clampi(new_hp, 0, 100): got_hit()
+	hp = clampi(new_hp, 0, 100)
+	%HPBar.value = hp
 
-func set_new_value(new_v) -> void:
-	if value > clampi(new_v, 0, 100):
-		if timer_await.time_left:
-			timer_await.stop()
-			await get_tree().process_frame
-		else:
-			var buf: float = bar_flow.value
-			timer_flow.stop()
-			bar_flow.value = buf
+func got_hit() -> void:
+	if timer_await.time_left:
+		timer_await.stop()
+		await get_tree().process_frame
+	else:
+		var buf: float = bar_flow_hp.value
+		timer_flow.stop()
+		bar_flow_hp.value = buf
 	timer_await.start()
-	value = clampi(new_v, 0, 100)
-	if bar_basic != null: bar_basic.value = value
 
-## Set's curve specificly for current amount of degreesed value.
 func set_curve() -> void:
-	incurve = Curve.new()
-	incurve.max_value = bar_flow.value
-	incurve.min_value = value
+	incurve = Curve.new()#curve
+	incurve.max_value = bar_flow_hp.value
+	incurve.min_value = hp
 	for p in range(0, curve.point_count):
-		incurve.add_point(Vector2(curve.get_point_position(p).x, curve.get_point_position(p).y * (incurve.max_value - incurve.min_value) + incurve.min_value), curve.get_point_left_tangent(p), curve.get_point_right_tangent(p), Curve.TANGENT_FREE, Curve.TANGENT_FREE)
+		incurve.add_point(Vector2(curve.get_point_position(p).x, curve.get_point_position(p).y * (incurve.max_value - incurve.min_value) + incurve.min_value), rad_to_deg(atan(curve.get_point_left_tangent(p))), rad_to_deg(atan(curve.get_point_right_tangent(p))))
+	incurve.bake()
 
-func _start_flow() -> void: ## Emits when [member timer_await] runs out.
+func start_flow() -> void:
 	set_curve()
 	await get_tree().process_frame
 	timer_flow.start()
 	while timer_flow.time_left:
-		bar_flow.value = incurve.sample((timer_flow.wait_time - timer_flow.time_left)/timer_flow.wait_time)
+		bar_flow_hp.value = incurve.sample_baked((timer_flow.wait_time - timer_flow.time_left)/timer_flow.wait_time)
 		await get_tree().process_frame
+
+#region Scene's junk
+func _on_hit_button_pressed() -> void: hp -= $HitXontainer/TextEdit.text.to_int()
